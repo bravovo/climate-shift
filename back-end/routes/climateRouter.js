a0a47eeada0bfa596e230b28060c0171ef4c16bb
bravovo <bogdan.nyschyi@gmail.com>
2025-01-25 13:32:54 +0200

    climateRouter. added city route to get city name from coordinates

--- back-end/routes/climateRouter.js
+++ back-end/routes/climateRouter.js
@@ -68,6 +68,57 @@ router.get("/coords", async (request, response) => {
     }
 });
 
+router.get('/city', async (request, response) => { 
+    const { lat, lng } = request.query;
+
+    const startTime = Date.now();
+    try {
+        const opencageResponse = await axios.get(
+            `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=${OPENCAGE_API}`
+        );
+
+        if (
+            opencageResponse.data.status.code === 200 &&
+            opencageResponse.data.results.length > 0
+        ) {
+            const place = opencageResponse.data.results[0];
+            console.log(
+                "Requests remaining",
+                opencageResponse.data.rate.remaining
+            );
+            console.log(place.components.city);
+            const pendingTime = requestPendingTime(startTime);
+            return response.status(200).send({
+                city: place.components.city,
+                time: { type: "seconds", value: pendingTime },
+            });
+        } else {
+            console.log("Status", opencageResponse.data.status.message);
+            console.log("total_results", opencageResponse.data.total_results);
+            const pendingTime = requestPendingTime(startTime);
+            return response.status(404).send({
+                message: "Міста із заданими координатами не знайдено",
+                time: { type: "seconds", value: pendingTime },
+            });
+        }
+    } catch (error) {
+        console.log(error.message);
+
+        const endTime = Date.now();
+        console.log(
+            `Request duration: ${(endTime - startTime) / 1000} seconds`
+        );
+
+        if (error.response.status.code === 402) {
+            console.log("hit free trial daily limit");
+            return response.status(402).send({
+                message: "Зараз неможливо дізнатись координати за назвою міста",
+            });
+        }
+        return response.status(500).send({ message: "Помилка на сервері" });
+    }
+});
+
 router.get("/daily", async (request, response) => {
     const startTime = Date.now();
     const { lat, lng } = request.query;
