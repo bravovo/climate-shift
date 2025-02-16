8ec920819dd711b533fd587ef2fdbfbc46f4b680
bravovo <bogdan.nyschyi@gmail.com>
2025-02-16 12:22:15 +0200

    added userSlice to save user data into state

---
+++ front-end/src/state/user/userSlice.js
@@ -0,0 +1,71 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    email: "",
+    lat: null,
+    lng: null,
+    lang: "",
+    loading: false,
+    feched: false,
+};
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        addUser: (state, action) => {
+            return { ...action.payload, loading: false };
+        },
+        logoutUser: () => {
+            return initialState;
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(checkUserExist.pending, (state) => {
+                state.loading = true;
+                state.fetched = false;
+                console.log("User session is pending");
+            })
+            .addCase(checkUserExist.fulfilled, (state, action) => {
+                return action.payload;
+            })
+            .addCase(checkUserExist.rejected, (state, action) => {
+                console.error(
+                    "Failed to fetch weather data:",
+                    action.error.message
+                );
+                return initialState;
+            });
+    },
+});
+
+export const checkUserExist = createAsyncThunk("user/getUser", async () => {
+    try {
+        const serverResponse = await axios.get(
+            "http://localhost:5000/api/auth/user",
+            { withCredentials: true }
+        );
+
+        if (serverResponse) {
+            return { ...serverResponse.data, loading: false, fetched: true };
+        }
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.data.message);
+            return {
+                message: error.response.data.message,
+                loading: false,
+                fetched: true,
+            };
+        } else {
+            console.log(error.message);
+            return { message: error.message, loading: false, fetched: true };
+        }
+    }
+});
+
+export const { addUser, logoutUser } = userSlice.actions;
+
+export default userSlice.reducer;
--- front-end/src/store.js
+++ front-end/src/store.js
@@ -3,7 +3,8 @@ import monthlyClimateDataReducer from "./state/monthlyClimateData/monthlyClimate
 import dataLangReducer from "./state/dataLang/dataLangSlice";
 import yearsClimateDataReducer from "./state/yearsClimateData/yearsClimateDataSlice";
 import coordsReducer from './state/coords/coordsSlice';
-import weatherSlice from "./state/weather/weatherSlice";
+import weatherReducer from "./state/weather/weatherSlice";
+import userReducer from './state/user/userSlice';
 
 export const store = configureStore({
     reducer: {
@@ -11,6 +12,7 @@ export const store = configureStore({
         yearsClimateData: yearsClimateDataReducer,
         dataLang: dataLangReducer,
         coords: coordsReducer,
-        weather: weatherSlice,
+        weather: weatherReducer,
+        user: userReducer
     },
 });
